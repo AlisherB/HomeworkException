4c47adea617f3199ca980b1025c3b73fb8f1989d
AlisherB <alisher_b_95@mail.ru>
2017-09-05 14:26:52 +0600

    HomeworkException
    Домашнее задание - Исключения(19.07.17-29.07.17)

---
+++ HomeworkException/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task1
+{
+    class Program
+    {
+        /*
+         1.	Перехватить исключение запроса к несуществующему веб ресурсу и вывести сообщение о том, что произошла ошибка. 
+         Программа должна завершиться без ошибок.
+         */
+        static void Main(string[] args)
+        {
+            Write("Введите веб-ресурс: ");
+            string web = ReadLine();
+            try
+            {
+                throw new Exception($"Несуществующий веб-ресурс!");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex);
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ HomeworkException/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task2
+{
+    class Program
+    {
+        /*
+         2.	Написать программу, которая обращается к элементам массива по индексу и выходит за его пределы. 
+         После обработки исключения вывести в финальном блоке сообщение о завершении обработки массива.
+        */
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 10, 25, 67, 74, 12 };
+            foreach(var i in arr)
+            {
+                Write(i + "\t");
+            }
+            try
+            {
+                WriteLine("\nВведите индекс:");
+                int index = Convert.ToInt32(ReadLine());
+                if (index > arr.Length)
+                {
+                    throw new IndexOutOfRangeException($"Индекс находится за пределами массива!");
+                }
+                else
+                {
+                    WriteLine(arr[index]);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                WriteLine(ex);
+            }
+            ReadLine();
+        }
+    }
+}
